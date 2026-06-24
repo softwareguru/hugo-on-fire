2f3d92bf747becffdd0b9978309abceac28b0f5c
Pedro Galvan <pedro@sg.com.mx>
2026-06-23 21:52:41 -0400

    Update entrypoint.sh for google application credentials
    Update to read service account key from google application credentials.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,28 @@ node -v
 firebase --version
 hugo version
 
-# Get Firebase Token
-if [ -z "${FIREBASE_TOKEN}" ]; then
-    echo "FIREBASE_TOKEN is missing." /
-    echo "You can get it using command 'firebase login:ci' and then set it in Github Secrets as FIREBASE_TOKEN variable."
-    exit 1
+# Export google application credentials
+if [ -z "$GCP_SA_KEY" ] && [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+  echo "Either FIREBASE_TOKEN or GCP_SA_KEY or GOOGLE_APPLICATION_CREDENTIALS is required to run commands with the firebase cli"
+  exit 126
+fi
+
+if [ -n "$GCP_SA_KEY" ]; then
+  if echo "$GCP_SA_KEY" | jq empty 2>/dev/null; then
+    echo "Storing GCP_SA_KEY in /opt/gcp_key.json"
+    echo "$GCP_SA_KEY" > /opt/gcp_key.json
+  else
+    echo "Storing the decoded GCP_SA_KEY in /opt/gcp_key.json"
+    echo "$GCP_SA_KEY" | base64 -d > /opt/gcp_key.json # If encoded base64 key, decode and save
+  fi
+
+  echo "Exporting GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json"
+  export GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json
 fi
 
 # Generate hugo site
 hugo
 
 # Deploy it on Firebase
-firebase use --token "${FIREBASE_TOKEN}"
-firebase deploy --non-interactive --token "${FIREBASE_TOKEN}"
+firebase use
+firebase deploy --non-interactive
